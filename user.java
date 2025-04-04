a5cdc913e64a1dc96c69c3a8639ca292cb6bd47a
brinda790 <bxa5451@mavs.uta.edu>
2025-04-04 18:59:30 -0500

    Update user.java

--- user.java
+++ user.java
@@ -8,17 +8,14 @@ public class UserPage {
         System.out.print("Enter Name: ");
         String name = scanner.nextLine();  // Read user input for name
         
-        System.out.print("Enter Date of Birth (YYYY-MM-DD): ");
-        String dob = scanner.nextLine();  // Read user input for date of birth
-        
         //Makesure both name and dob are provided
-        if (name.isEmpty() || dob.isEmpty()) {
-            System.out.println("Error!!!!\nName and Date of Birth are required.");
+        if (name.isEmpty()) {
+            System.out.println("Error!!!!\nName  is required.");
         } else {
             
             System.out.println("\nUser Details:");
             System.out.println("Name: " + name);
-            System.out.println("Date of Birth: " + dob);
+            
             
             
             System.out.println("Let's Enter your income information now\n");
