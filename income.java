d53a095f70b87ab0415644e52b60498f4c62c89b
Ayush <axs8308@mavs.uta.edu>
2025-04-04 19:35:52 -0500

    Add files via upload
    Add income

---
+++ income.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class income {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many income sources do you have? ");
+        int count = scanner.nextInt();
+
+        double totalIncome = 0;
+
+        for (int i = 1; i <= count; i++) {
+            System.out.print("Enter income for source " + i + ": ");
+            double income = scanner.nextDouble();
+            totalIncome += income;
+        }
+
+        System.out.println("Your total income is: $" + totalIncome);
+    }
+}
