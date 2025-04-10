f669212585f0f8d7f15ec76300944c73ba62d056
brinda790 <bxa5451@mavs.uta.edu>
2025-04-10 16:12:37 -0500

    Update ExpenseTracker.java

--- ExpenseTracker.java
+++ ExpenseTracker.java
@@ -4,19 +4,20 @@ public class ExpenseTracker {
     private double saving;
 
     public ExpenseTracker(double rent, double groceries, double tution, double shopping, double recreation,
-            double commute, double remaining) {
+            double commute, double health, double remaining) {
         this.rent = rent;
         this.groceries = groceries;
         this.tution = tution;
         this.shopping = shopping;
         this.recreation = recreation;
         this.commute = commute;
+        this.health = health;
         this.remaining = remaining;
 
     }
 
     public double totalExpensesExcludingSaving() {
-        return rent + groceries + tution + shopping + recreation + commute + remaining;
+        return rent + groceries + tution + shopping + recreation + commute + health + remaining;
     }
     public void setSaving(double saving){
         this.saving =saving;
