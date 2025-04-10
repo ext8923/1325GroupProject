e404ad29bb506186914e667fc0de0ce7f0ca9f5c
brinda790 <bxa5451@mavs.uta.edu>
2025-04-10 16:19:44 -0500

    Create DetailExpenseTracker.java

---
+++ DetailExpenseTracker.java
@@ -0,0 +1,11 @@
+public class DetailExpenseTracker extends ExpenseTracker{
+  public DetailedexpenseTracker(double rent, double groceries, double tution, double shopping, double recreation, double commute, double health, double remaining){
+    super(rent, groceries, tution, shopping, recreation, commute, health, remaining);
+  }
+
+  @Override
+  public double totalExpenseWithoutSaving(){
+    return super.totalExpenseWithoutSaving();
+  }
+}
+  
