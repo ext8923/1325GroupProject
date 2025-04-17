f200455d1b7ecddea6d65ee9af2584275dfee2e3
ext8923 <ext8923@mavs.uta.edu>
2025-04-17 19:54:25 +0000

    push

--- DetailExpenseTracker.java
+++ DetailExpenseTracker.java
@@ -1,17 +1,16 @@
-public class DetailExpenseTracker extends ExpenseTracker{
-  public DetailedExpenseTracker(double rent, double groceries, double tution, double shopping, double recreation, double commute, double health, double remaining){
-    super(rent, groceries, tution, shopping, recreation, commute, health);
-  }
+public class DetailExpenseTracker extends ExpenseTracker {
+    public DetailExpenseTracker(double rent, double groceries, double tuition, double shopping, double recreation, 
+                                double commute, double health, double remaining) {
+        super(rent, groceries, tuition, shopping, recreation, commute, health);
+    }
 
-  @Override
-  public double totalExpenseExcludingSaving(){
-    double total = super.totalExpensesExcludingSaving();
-     double savings = getSaving();
-    System.out.println("Savings: " + savings);
-    double totalWithoutSavings = total - savings;
-    return super.totalExpenseExcludingSaving();
-    System.out.println("Total expenses excluding Savings:" +totalWithoutSavings);
-    return totalWithoutSavings;
-  }
+    @Override
+    public double totalExpenseExcludingSaving() {
+        double total = super.totalExpensesExcludingSaving();
+        double savings = getSaving();  // Ensure this method exists in ExpenseTracker
+        System.out.println("Savings: " + savings);
+        double totalWithoutSavings = total - savings;
+        System.out.println("Total expenses excluding Savings: " + totalWithoutSavings);
+        return totalWithoutSavings;  // Fixed issue: No duplicate method call
+    }
 }
-  
--- ExpenseTracker.java
+++ ExpenseTracker.java
@@ -1,23 +1,19 @@
-public class ExpenseTracker extends Finance{
+public class DetailExpenseTracker extends Finance {
+    double rent, groceries, tuition, shopping, recreation, commute, health, remaining;
 
-    double rent, groceries, tution, shopping, recreation, commute, health ;
-   
-
-    public ExpenseTracker(double rent, double groceries, double tution, double shopping, double recreation,
-            double commute, double health) {
+    public DetailExpenseTracker(double rent, double groceries, double tuition, double shopping, double recreation, 
+                                double commute, double health, double remaining) {
         this.rent = rent;
         this.groceries = groceries;
-        this.tution = tution;
+        this.tuition = tuition;
         this.shopping = shopping;
         this.recreation = recreation;
         this.commute = commute;
         this.health = health;
-       ;
-
+        this.remaining = remaining;
     }
 
     public double totalExpensesExcludingSaving() {
-        return rent + groceries + tution + shopping + recreation + commute + health;
+        return rent + groceries + tuition + shopping + recreation + commute + health;
     }
-
-    }
+}
